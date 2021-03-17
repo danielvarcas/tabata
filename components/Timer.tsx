a8c231e1bbecdcff1d07a126d1a1758366d64d3f
danielvarcas <daniel.varcas@gmail.com>
2021-03-17 16:33:19 +0000

    use Duration from Luxon

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,17 +1,41 @@
 import * as React from "react";
-import { Input, Button } from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
 import { useForm } from "react-hook-form";
+import * as Luxon from "luxon";
 
 export function Timer() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data: any) => console.log(data);
+  const [duration, setDuration] = React.useState<Luxon.Duration | null>(null);
+
+  function onSubmit(data) {
+    const [hours, minutes, seconds] = data.timer.split(":");
+    const duration = Luxon.Duration.fromObject({ hours, minutes, seconds });
+
+    setDuration(duration);
+  }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Input type="time" name="timer" inputRef={register} />
-      <Button type="submit" variant="contained">
-        Go
-      </Button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          type="time"
+          name="timer"
+          inputRef={register}
+          defaultValue="00:00:00"
+          inputProps={{ step: 1 }}
+        />
+
+        <Button type="submit" variant="contained">
+          Go
+        </Button>
+      </form>
+
+      {duration !== null && (
+        <div>
+          <p>{duration.toFormat("mm : ss")} </p>
+          <p>{duration.as("seconds")}</p>
+        </div>
+      )}
+    </>
   );
 }
